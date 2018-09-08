ec0db2e1d7b7f3415b75c2740b83e1c15062ba63
silvercobraa <silver.cobra1n@gmail.com>
2018-09-08 02:41:34 -0300

    Se optimizó el cálculo de movimientos...
    Pero se echó a perder la puntuación

--- 2048.cpp
+++ 2048.cpp
@@ -8,11 +8,13 @@ using namespace std;
 // Se almacenan solo los exponentes de los valores de cada celda,
 // Se ocupa un entero de 64 bits para el estado, donde 16 bits consecutivos
 // corresponden a una fila
-static int move_table[1 << 16];
-static int score_table[1 << 16];
+static int l_lookup[1 << 16];
+static int r_lookup[1 << 16];
+static int score_lookup[1 << 16];
 
 class Puzzle2048 {
 private:
+
 public:
 	uint64_t state;
 	long score;
@@ -26,7 +28,7 @@ public:
 		else if (source == destiny) {
 			destiny += 1;
 			source = 0;
-			score_table[row] += (1 << destiny);
+			score_lookup[row] += (1 << destiny);
 			return true;
 		}
 		return false;
@@ -37,52 +39,61 @@ public:
 		int random = rand() % 10;
 		return random == 0 ? 2 : 1;
 	}
-	void place_random_tile() {
+	uint64_t place_random_tile(uint64_t st) {
 		vector<pair<int, int>> empty_cells;
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				if (get(i, j) == 0) {
+				if (get(st, i, j) == 0) {
 					empty_cells.push_back({i, j});
 				}
 			}
 		}
 		if (empty_cells.empty()) {
-			cerr << "Game Over!" << endl;
-			return;
+			return st;
 		}
 		int random = rand() % empty_cells.size();
 		auto cell = empty_cells[random];
-		set(cell.first, cell.second, random_value());
+		return set(st, cell.first, cell.second, random_value());
 	}
 
 	Puzzle2048 () {
 		this->state = 0L;
 		this->score = 0L;
-		place_random_tile();
-		place_random_tile();
+		this->state = place_random_tile(this->state);
+		this->state = place_random_tile(this->state);
 	}
 	long get_score() {
-		return this->score;
+		long ret = 0;
+		int i = 1;
+		for (uint64_t st = this->state; st != 0; st >>= 4) {
+			int exp = st & 0xF;
+			ret += exp >= 3 ? (1 << exp) : 0;
+			// ret += exp >= 4 ? exp*(1 << exp) : 0;
+			// ret += exp >= 2 ? (1 << exp)*(1 << exp) : 0;
+			// ret += 1 << (exp + i);
+			i++;
+		}
+		return ret;
+		// return this->score;
 	}
 	uint64_t get_state() {
 		return this->state;
 	}
-	bool empty(int i, int j) {
-		return get(i, j) == 0;
-	}
-	uint64_t get(int i, int j) {
+
+	uint64_t get(uint64_t st, int i, int j) {
 		// assert(0 <= i and i < 4 and 0 <= j and j < 4);
 		int offset = 16*i + 4*j;
 		uint64_t mask = 0xF;
-		return this->state >> offset & mask;
+		return st >> offset & mask;
 	}
 
-	void set(int i, int j, uint64_t value) {
+	uint64_t set(uint64_t st, int i, int j, uint64_t value) {
 		// assert(0 <= i and i < 4 and 0 <= j and j < 4 and 0 <= value and value < 16);
 		int offset = 16*i + 4*j;
 		uint64_t mask = 0xF;
-		this->state &= ~(mask << offset); // seteo a cero los bits donde quiero colcar el nuevo valor
-		this->state |= value << offset; // coloco el nuevo valor
+		st &= ~(mask << offset); // seteo a cero los bits donde quiero colcar el nuevo valor
+		st |= value << offset; // coloco el nuevo valor
+		return st;
 	}
 
 	static void precompute_tables() {
@@ -99,7 +110,10 @@ public:
 			merge0 = merge0 or merge1 ? true : Puzzle2048::merge(row, b1, b0);
 			merge1 = merge1 or merge2 ? true : Puzzle2048::merge(row, b2, b1);
 			merge0 = merge0 or merge1 ? true : Puzzle2048::merge(row, b1, b0);
-			move_table[row] = ((b3 & 0xF) << 12) | ((b2 & 0xF) << 8) | ((b1 & 0xF) << 4) | ((b0 & 0xF) << 0);
+			int new_row = ((b3 & 0xF) << 12) | ((b2 & 0xF) << 8) | ((b1 & 0xF) << 4) | ((b0 & 0xF) << 0);
+			l_lookup[row] = new_row;
+			r_lookup[Puzzle2048::reverse(row)] = reverse(new_row);
+			// TODO: crear una lookup table para la fila reversa
 		}
 	}
 	void print() {
@@ -116,169 +130,104 @@ public:
 		}
 		cout << endl;
 	}
-	bool merge(int i1, int j1, int i2, int j2) {
-		int v1 = get(i1, j1);
-		int v2 = get(i2, j2);
-		if (v1 == 0) {
-			set(i1, j1, v2);
-			set(i2, j2, 0);
-			return false;
-		}
-		else if (v1 == v2) {
-			set(i1, j1, v1 + 1);
-			score += (2 << (v1 + 1));
-			// cout << "score +" << (1 << (v1 + 1)) << endl;
-			set(i2, j2, 0);
-			return true;
-		}
-		return false;
-	}
 	// combino las celdas eligiendo los pares de manera similar que se eligen
 	// en bubble-sort
 	// los booleanos me indican si ya mezcle algunas celdas, asi no
 	// las combino de nuevo
-	bool move_left() {
+	uint64_t left() {
 		// cout << "left" << endl;
-		uint64_t prev_state = this->state;
-		bool merge0, merge1, merge2;
-		for (int row = 0; row < 4; row++) {
-			merge0 = merge1 = merge2 = false;
-			merge0 = merge(row, 0, row, 1);
-			merge1 = merge(row, 1, row, 2);
-			merge2 = merge(row, 2, row, 3);
-			merge0 = merge0 or merge1 ? true : merge(row, 0, row, 1);
-			merge1 = merge1 or merge2 ? true : merge(row, 1, row, 2);
-			merge0 = merge0 or merge1 ? true : merge(row, 0, row, 1);
+		uint64_t new_state = 0L;
+		for (size_t i = 0; i < 4; i++) {
+			int row = (this->state >> (16*i)) & 0xFFFF;
+			uint64_t r = l_lookup[row];
+			new_state |= r << (16*i);
 		}
-		// print();
-		// printf("%016lx\n", prev_state);
-		// printf("%016lx\n", this->state);
-		if (prev_state != this->state) {
-			place_random_tile();
-			// print();
-			return true;
-		}
-		return false;
+		return new_state;
 	}
 
-	bool move_right() {
+	static int reverse(int word) {
+		// printf("%04lx\n", word);
+		int b0 = (word >> 0) & 0xF;
+		int b1 = (word >> 4) & 0xF;
+		int b2 = (word >> 8) & 0xF;
+		int b3 = (word >> 12) & 0xF;
+		int ret = (b0 << 12) | (b1 << 8) | (b2 << 4) | (b3 << 0);
+		// printf("%04lx\n", ret);
+		return ret;
+	}
+	uint64_t right() {
 		// cout << "right" << endl;
-		uint64_t prev_state = this->state;
-		bool merge0, merge1, merge2;
-		for (int row = 0; row < 4; row++) {
-			merge0 = merge1 = merge2 = false;
-			merge0 = merge(row, 3, row, 2);
-			merge1 = merge(row, 2, row, 1);
-			merge2 = merge(row, 1, row, 0);
-			merge0 = merge0 or merge1 ? true : merge(row, 3, row, 2);
-			merge1 = merge1 or merge2 ? true : merge(row, 2, row, 1);
-			merge0 = merge0 or merge1 ? true : merge(row, 3, row, 2);
+		uint64_t new_state = 0L;
+		for (size_t i = 0; i < 4; i++) {
+			int row = (this->state >> (16*i)) & 0xFFFF;
+			uint64_t r = r_lookup[row];
+			new_state |= r << (16*i);
 		}
-		// print();
-		// printf("%016lx\n", prev_state);
-		// printf("%016lx\n", this->state);
-		if (prev_state != this->state) {
-			place_random_tile();
-			// print();
-			return true;
+		return new_state;
+	}
+	uint64_t transpose(uint64_t st) {
+		// TODO: implementar esto
+		uint64_t new_state = 0L;
+		// printf("! %016lx\n", st);
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				new_state |= ((st >> 16*i + 4*j) & 0xF) << (16*j + 4*i);
+				// printf("%016lx\n", new_state);
+			}
 		}
-		return false;
+		// printf("! %016lx\n", new_state);
+		return new_state;
+	}
+	uint64_t up() {
+		uint64_t tr = transpose(this->state);
+		uint64_t new_state = 0L;
+		for (size_t i = 0; i < 4; i++) {
+			int row = (tr >> (16*i)) & 0xFFFF;
+			uint64_t r = l_lookup[row];
+			new_state |= r << (16*i);
+			// printf("up: %016lx\n", new_state);
+		}
+		return transpose(new_state);
 	}
 
-	bool move_up() {
-		// cout << "up" << endl;
-		uint64_t prev_state = this->state;
-		bool merge0, merge1, merge2;
-		for (int col = 0; col < 4; col++) {
-			merge0 = merge1 = merge2 = false;
-			merge0 = merge(0, col, 1, col);
-			merge1 = merge(1, col, 2, col);
-			merge2 = merge(2, col, 3, col);
-			merge0 = merge0 or merge1 ? true : merge(0, col, 1, col);
-			merge1 = merge1 or merge2 ? true : merge(1, col, 2, col);
-			merge0 = merge0 or merge1 ? true : merge(0, col, 1, col);
-		}
-		// print();
-		// printf("%016lx\n", prev_state);
-		// printf("%016lx\n", this->state);
-		if (prev_state != this->state) {
-			place_random_tile();
-			// print();
-			return true;
-		}
-		return false;
-	}
-
-	bool move_down() {
+	uint64_t down() {
 		// cout << "down" << endl;
-		uint64_t prev_state = this->state;
-		bool merge0, merge1, merge2;
-		for (int col = 0; col < 4; col++) {
-			merge0 = merge1 = merge2 = false;
-			merge0 = merge(3, col, 2, col);
-			merge1 = merge(2, col, 1, col);
-			merge2 = merge(1, col, 0, col);
-			merge0 = merge0 or merge1 ? true : merge(3, col, 2, col);
-			merge1 = merge1 or merge2 ? true : merge(2, col, 1, col);
-			merge0 = merge0 or merge1 ? true : merge(3, col, 2, col);
+		uint64_t tr = transpose(this->state);
+		uint64_t new_state = 0L;
+		for (size_t i = 0; i < 4; i++) {
+			int row = (tr >> (16*i)) & 0xFFFF;
+			uint64_t r = r_lookup[row];
+			new_state |= r << (16*i);
 		}
-		// print();
-		// printf("%016lx\n", prev_state);
-		// printf("%016lx\n", this->state);
-		if (prev_state != this->state) {
-			place_random_tile();
-			// print();
-			return true;
-		}
-		return false;
+		return transpose(new_state);
 	}
-	bool random_move() {
+
+	uint64_t random_move() {
 		int r = rand() % 4;
 		if (r == 0) {
-			return move_up();
+			return up();
 		}
 		else if (r == 1) {
-			return move_left();
+			return left();
 		}
 		else if (r == 2) {
-			return move_down();
-		}
-		else if (r == 3) {
-			return move_right();
+			return down();
 		}
 		else {
-			return false;
+			return right();
 		}
 	}
+
 	bool can_move() {
-		Puzzle2048 u = *this;
-		Puzzle2048 l = *this;
-		Puzzle2048 d = *this;
-		Puzzle2048 r = *this;
-		u.move_up();
-		l.move_left();
-		d.move_down();
-		r.move_right();
-		return !(state == u.get_state() and state == l.get_state() and state == d.get_state() and state == r.get_state());
+		return state != up() or state != left() or state != down() or state != right();
 	}
 };
 
-long simulate(Puzzle2048& p, int iterations) {
-	Puzzle2048 p2 = p;
-	int i = 0;
-	bool ret = true;
-	do {
-		i++;
-		ret = p2.random_move();
-	} while (i < iterations and ret);
-	// cout << p2.get_score() << endl;
-	return p2.get_score();
-}
-
-long simulate2(Puzzle2048& p) {
+long simulate(Puzzle2048& p) {
 	Puzzle2048 p2 = p;
 	while(p2.can_move()) {
-		p2.random_move();
+		p2.state = p2.random_move();
+		p2.state = p2.place_random_tile(p2.state);
 	}
 	return p2.get_score();
 }
@@ -288,19 +237,31 @@ void pure_mcts(Puzzle2048& p, int simulations) {
 	Puzzle2048 pl = p;
 	Puzzle2048 pd = p;
 	Puzzle2048 pr = p;
-	pu.move_up();
-	pl.move_left();
-	pd.move_down();
-	pr.move_right();
+	pu.state = p.up();
+	pl.state = p.left();
+	pd.state = p.down();
+	pr.state = p.right();
 	long ansu = 0;
 	long ansl = 0;
 	long ansd = 0;
 	long ansr = 0;
 	for (int i = 0; i < simulations; i++) {
-		ansu += simulate2(pu);
-		ansl += simulate2(pl);
-		ansd += simulate2(pd);
-		ansr += simulate2(pr);
+		if (pu.state != p.state) {
+			pu.state = pu.place_random_tile(pu.state);
+			ansu += simulate(pu);
+		}
+		if (pl.state != p.state) {
+			pl.state = pl.place_random_tile(pl.state);
+			ansl += simulate(pl);
+		}
+		if (pd.state != p.state) {
+			pd.state = pd.place_random_tile(pd.state);
+			ansd += simulate(pd);
+		}
+		if (pr.state != p.state) {
+			pr.state = pr.place_random_tile(pr.state);
+			ansr += simulate(pr);
+		}
 	}
 	cout << "u: " << ansu << endl;
 	cout << "l: " << ansl << endl;
@@ -310,20 +271,21 @@ void pure_mcts(Puzzle2048& p, int simulations) {
 	cout << "best score: " << max << endl;
 	if (max == ansu) {
 		cout << "UP" << endl;
-		p.move_up();
+		p.state = p.up();
 	}
 	else if (max == ansl) {
 		cout << "LEFT" << endl;
-		p.move_left();
+		p.state = p.left();
 	}
 	else if (max == ansd) {
 		cout << "DOWN" << endl;
-		p.move_down();
+		p.state = p.down();
 	}
 	else if (max == ansr) {
 		cout << "RIGHT" << endl;
-		p.move_right();
+		p.state = p.right();
 	}
+	p.state = p.place_random_tile(p.state);
 }
 
 int main(int argc, char const *argv[]) {
@@ -332,12 +294,12 @@ int main(int argc, char const *argv[]) {
 	// simulate(p, 1000);
 	Puzzle2048::precompute_tables();
 	p.print();
-	// for (int i = 0; p.can_move(); i++) {
-	// 	pure_mcts(p, 1000);
-	// 	cout << "current score: " << p.get_score() << endl;
-	// 	p.print();
-	// }
-	// cout << "GAME OVER!" << endl;
+	for (int i = 0; p.can_move(); i++) {
+		pure_mcts(p, 3000);
+		cout << "current score: " << p.get_score() << endl;
+		p.print();
+	}
+	cout << "GAME OVER!" << endl;
 
 	return 0;
 }
