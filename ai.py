a2a17b8cd0bb840317f34c116158208ac1a710f0
silvercobraa <silver.cobra1n@gmail.com>
2018-10-02 21:32:51 -0300

    MCTS

--- ai.py
+++ ai.py
@@ -1,7 +1,7 @@
 from puzzle2048 import Puzzle2048
 
 class AI(object):
-    EXPECTIMAX_DEPTH = 4
+    EXPECTIMAX_DEPTH = 5
     INF = 99999999
 
     """docstring for AI."""
@@ -23,13 +23,14 @@ class AI(object):
                     state2 = state
                     state4 = state
                     state2 ^= ((state >> offset) & 0xF) << offset
-                    state4 ^= ((state >> offset) & 0xF) << offset
+                    # state4 ^= ((state >> offset) & 0xF) << offset
                     state2 ^= 1 << offset
-                    state4 ^= 2 << offset
+                    # state4 ^= 2 << offset
                     score2 = AI.__choice(state2, depth - 1)
-                    score4 = AI.__choice(state4, depth - 1)
+                    # score4 = AI.__choice(state4, depth - 1)
+                    expected_score += 1 * score2
                     expected_score += 0.9 * score2
-                    expected_score += 0.1 * score4
+                    # expected_score += 0.1 * score4
         return 0 if total_weight == 0 else expected_score / total_weight
 
 
@@ -65,6 +66,37 @@ class AI(object):
         assert(best_state != 0)
         # print(move_name[best_move])
         return Puzzle2048.place_random_tile(best_state), game_score
+
+    def __simulate(state):
+        score = 0
+        while Puzzle2048.can_move(state):
+            new_state, sc = Puzzle2048.random_move(state)
+            if new_state != state:
+                state = Puzzle2048.place_random_tile(new_state)
+                score += sc
+        return state, score
+
+    def __simulations(state, simulations):
+        branch_score = 0
+        for i in range(simulations):
+            sim_state, sim_score = AI.__simulate(state)
+            branch_score += sim_score
+        return branch_score
+
+    def pure_mcts(state, simulations):
+        best_score = -AI.INF
+        ans = (state, 0)
+        for move in Puzzle2048.moves:
+            new_state, sc = move(state)
+            if new_state != state:
+                new_state = Puzzle2048.place_random_tile(new_state)
+                branch_score = AI.__simulations(new_state, simulations)
+                if branch_score > best_score:
+                    best_score = branch_score
+                    ans = (new_state, sc)
+        return ans
+
+
 
 def main():
     Puzzle2048.precompute_tables()
